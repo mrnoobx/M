2eae37af42f012edf51f49ba61fcb3c092bc9a3f
mrnoobx <154804791+mrnoobx@users.noreply.github.com>
2025-04-27 20:22:47 +0530

    Update status.py

--- bot/modules/status.py
+++ bot/modules/status.py
@@ -264,7 +264,7 @@ def bot_sys_stats():
     bmsg += f"S: {swap}% | "
     bmsg += f"D: {disk}%\n\n"
     bmsg += f"Bandwidth Used: {traf}\n"
-    bmsg += "Join @JetMirror"
+    bmsg += "Join @noob_project"
     return bmsg
 
 
